b19f2785328e9e06a2e1812b8de599b15e6df9d3
Tyree Zacharopoulos <tyree@tyree.ca>
2023-04-11 14:46:56 -0700

    TZAPI-2-Fastify-Generate-Project

--- src/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-const test0Route = require("./tests/test0.route");
-
-// v1 Router
-const v1Router = (instance, options, done) => {
-	instance.register(test0Route, { prefix: "/tests" });
-	done();
-};
-
-module.exports = v1Router;
