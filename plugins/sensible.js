9b7d2fc536104c44a73273091015731cb0d7d1ad
Tyree Zacharopoulos <tyree@tyree.ca>
2023-04-11 15:25:28 -0700

    TZAPI-2 Plugin Changes

--- plugins/sensible.js
+++ plugins/sensible.js
@@ -1,14 +1,9 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
+const fp = require("fastify-plugin");
 
-/**
- * This plugins adds some utilities to handle http errors
- *
- * @see https://github.com/fastify/fastify-sensible
- */
 module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/sensible'), {
-    errorHandler: false
-  })
-})
+	fastify.register(require("@fastify/sensible"), {
+		errorHandler: false,
+	});
+});
--- plugins/support.js
+++ plugins/support.js
@@ -1,12 +1,9 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
-
-// the use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
+const fp = require("fastify-plugin");
 
 module.exports = fp(async function (fastify, opts) {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
-})
+	fastify.decorate("someSupport", function () {
+		return "hugs";
+	});
+});
