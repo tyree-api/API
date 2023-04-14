89272ea1c27436fcd2e9b98481958943b0addb62
Tyree Zacharopoulos <tyree@tyree.ca>
2023-04-14 20:22:05 +0000

    Pushops WWT endpoint

--- routes/v1/pushops/scheduling.js
+++ routes/v1/pushops/scheduling.js
@@ -24,4 +24,26 @@ module.exports = async function (fastify, opts) {
 			reply.send(response);
 		}
 	);
+
+	fastify.get(
+		"/wwt",
+		{
+			// preValidation: fastify.authenticate,
+			// preHandler: [fastify.guard.scope(["read:schedules"])],
+			config: {
+				rateLimit: {
+					max: 10,
+					timeWindow: "30 seconds",
+				},
+			},
+		},
+		async function (request, reply) {
+			const response = await pushops.getWhosWorkingToday(
+				fastify.secrets.pushOpsapiaddr,
+				fastify.secrets.pushOpsusername,
+				fastify.secrets.pushOpspassword
+			);
+			reply.send(response);
+		}
+	);
 };
